a58068ac26400a6d2ed4a5ee8cc06591e2c6912f
DSaurus <2238454358@qq.com>
2024-01-01 03:23:29 +0800

    add version

--- __init__.py
+++ __init__.py
@@ -1,3 +1,17 @@
+import threestudio
+from packaging.version import Version
+
+if hasattr(threestudio, "__version__") and Version(threestudio.__version__) >= Version(
+    "0.2.2"
+):
+    pass
+else:
+    if hasattr(threestudio, "__version__"):
+        print(f"[INFO] threestudio version: {threestudio.__version__}")
+    raise ValueError(
+        "threestudio version must be >= 0.2.2, please update threestudio by pulling the latest version from github"
+    )
+
 from .guidance import (
     diffusers_deep_floyd_guidance,
     diffusers_guidance,
